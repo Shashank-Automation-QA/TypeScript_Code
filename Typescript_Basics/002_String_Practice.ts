ae64090869d4a9199e175305b60b12ee4702488f
shashank-automation-qa <officialsinghshashank091@gmail.com>
2026-08-13 23:13:38 +0530

    adding type script project with theory and string data details

---
+++ Typescript_Basics/002_String_Practice.ts
@@ -0,0 +1,16 @@
+//Syntax:-
+
+//var_declarition var_name: data_tyep = data;
+
+const user_name: string = "singh";
+console.log(user_name);
+
+//*************************************   String Methods *****************************************************/
+
+// ── String methods you will use in testing ──
+const status = 'pass';
+console.log(status.toLowerCase());   // 'pass'
+console.log(status.toUpperCase());   // 'pass'
+console.log(status.includes('P'));    // true
+console.log(status.startsWith('P')); // true
+console.log(status.length);          // 4
